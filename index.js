fe5423a77173595d14638c60f664ea6b5ed7c375
leiling.cp <leiling.cp@alibaba-inc.com>
2018-08-15 11:18:17 +0800

    fixed .fusion file

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Icon = require('./lib/icon/lib/index.js');
 
 const Vext = { Button: Button.default, Icon: Icon.default };
 
-Vext.version = '0.0.3';
+Vext.version = '0.0.4';
 
 module.exports.default = module.exports = Vext;
