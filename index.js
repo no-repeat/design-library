47e9f3b5768995d6eb8ef5e02476cb3f4f800c83
leiling.cp <leiling.cp@alibaba-inc.com>
2018-08-17 15:03:30 +0800

    fixed .fusion file and change some sass doc

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Icon = require('./lib/icon/lib/index.js');
 
 const Vext = { Button: Button.default, Icon: Icon.default };
 
-Vext.version = '0.0.7';
+Vext.version = '0.0.8';
 
 module.exports.default = module.exports = Vext;
