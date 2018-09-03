2daa100325ab416aac0ef690db3fbb1a80380864
leiling.cp <leiling.cp@alibaba-inc.com>
2018-09-03 19:25:02 +0800

    update sass

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Icon = require('./lib/icon/lib/index.js');
 
 const Vext = { Button: Button.default, Icon: Icon.default };
 
-Vext.version = '0.0.13';
+Vext.version = '0.0.14';
 
 module.exports.default = module.exports = Vext;
