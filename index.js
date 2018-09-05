590f54ececa3ce2803d84046741211877e0d6c19
leiling.cp <leiling.cp@alibaba-inc.com>
2018-09-05 17:52:32 +0800

    fixed font weight

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Icon = require('./lib/icon/lib/index.js');
 
 const Vext = { Button: Button.default, Icon: Icon.default };
 
-Vext.version = '0.0.16';
+Vext.version = '0.0.17';
 
 module.exports.default = module.exports = Vext;
