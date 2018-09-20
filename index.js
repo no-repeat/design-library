d6d688ccb6604d77e9459c4513b1a57af4d89d21
leiling.cp <leiling.cp@alibaba-inc.com>
2018-09-20 13:43:45 +0800

    fixed all

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const Icon = require('./lib/icon/lib/index.js');
 
 const Vext = { Button: Button.default, Icon: Icon.default };
 
-Vext.version = '0.0.24';
+Vext.version = '0.0.25';
 
 module.exports.default = module.exports = Vext;
